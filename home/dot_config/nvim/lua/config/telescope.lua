f1d31511103c74ce1854b69330ad43b265d362cd
rbnbb <rbnbb@users.noreply.github.com>
2026-01-09 11:59:38 +0100

    fix(nvim): improve telescope mapping

--- home/dot_config/nvim/lua/config/telescope.lua
+++ home/dot_config/nvim/lua/config/telescope.lua
@@ -1,6 +1,21 @@
 local builtin = require('telescope.builtin')
+local actions = require('telescope.actions')
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+require('telescope').setup{
+    defaults = {
+        mappings = {
+            i = {
+                ["<A-q>"] = function(prompt_bufnr)
+                    actions.send_selected_to_qflist(prompt_bufnr)
+                    actions.open_qflist(prompt_bufnr)
+                end,
+                ["<M-q>"] = false, -- disables the default mapping
+            }
+        }
+    }
+}
